1fb3b537f28693b08719ba0dd6f25865a18ee68a
Dark LinK <tomas.baez91@gmail.com>
2025-01-15 15:19:50 +0000

    Update Gimnasio.java

--- Gimnasio.java
+++ Gimnasio.java
@@ -2,7 +2,8 @@
 public class Gimnasio {
     private String nombre;
     private String[] actividades = {"Yoga", "Boxeo", "Funcional"};
-    private int contadorId = 1; 
+    private int contadorId = 00001; // Contador para los IDs de los usuarios
+
     public Gimnasio(String nombre) {
         this.nombre = nombre;
     }
@@ -11,36 +12,52 @@ public class Gimnasio {
         for (String actividad : actividades) {
             System.out.println("- " + actividad);
         }
+
     }
-    public void registrarUsuario(String nombreUsuario) {
-        Usuario nuevoUsuario = new Usuario(nombreUsuario, contadorId++);
-        System.out.println("Usuario " + nuevoUsuario.getNombre() + " registrado en el gimnasio " + nombre + " con ID: " + nuevoUsuario.getId() );
+
+    public void registrarUsuario(String nombreUsuario, String actividad) {
+        Usuario nuevoUsuario = new Usuario(nombreUsuario, contadorId++, actividad);
+        System.out.println("Usuario " + nuevoUsuario.getNombre() + " registrado en el gimnasio " + nombre + " con ID: " + nuevoUsuario.getId() + " y actividad: " + nuevoUsuario.getActividad() );
     }
     public static void main(String[] args) {
         Gimnasio miGimnasio = new Gimnasio("FitLife Gym");
         miGimnasio.mostrarActividades();
-        miGimnasio.registrarUsuario("Juan Perez");
-        miGimnasio.registrarUsuario("Ana Santana");
+        miGimnasio.registrarUsuario("Juan Perez", "Boxeo");
+        miGimnasio.registrarUsuario("Ana Santana", "Yoga");
     }
 }
 class Usuario {
     protected String nombre;
     protected int id;
+    protected String actividad;
 
-    public Usuario(String nombre, int id) {
+    public Usuario(String nombre, int id, String actividad) {
         this.nombre = nombre;
         this.id = id;
+        this.actividad = actividad;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
+    public String getActividad() {
+        return actividad;
     }
+
+    public void setActividad(String actividad) {
+        this.actividad = actividad;
+    }
+}
